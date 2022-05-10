503755bf519181f61cfe0f633caed42360a79f57
pavbyte <pavbyte@gmail.com>
2022-05-11 04:06:33 +0530

    added id based output on api

---
+++ middleware/logger.js
@@ -0,0 +1,8 @@
+const moment=require('moment')
+
+const logger=(req,res,next) => {
+    console.log(`${req.protocol}://${req.hostname}${req.originalUrl}:${moment().format()}`);
+    next();
+}
+
+module.exports=logger;
--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const path=require('path');
 const members=require('./Members')
-const moment=require('moment')
-
+const logger=require('./middleware/logger')
 
 const app=express();
 
-const logger = (req,res,next) => {
-    console.log(`${req.protocol}//${req.hostname}${req.originalUrl}:${moment().format()}`);
-    next();
-}
 
 app.use(logger);
 
@@ -17,8 +12,14 @@ const PORT= process.env.PORT || 3300;
 
 app.use(express.static(path.join(__dirname,'public')))
 
+
 app.get('/api/members',(req,res)=> {
     res.json(members);
+})
+
+app.get('/api/members/:id',(req,res) => {
+    // res.send(req.params.id)
+    res.json(members.filter(member => member.id === parseInt(req.params.id)))
 })
 
 
